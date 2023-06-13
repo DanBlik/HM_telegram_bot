27e8852a7c451427a58376ead194524f6980b500
Бликов Данила Дмитриевич <DDBlikov@sberbank.ru>
2023-06-13 10:01:30 +0300

    cosmetic improvement

--- handlers/list.js
+++ handlers/list.js
@@ -5,7 +5,7 @@ const list = async ({ ctx, database }) => {
   console.log(ctx.message)
 
   if (sprintNamesList.length !== 0) {
-    ctx.replyWithHTML(`Список названий:\n\n${sprintNamesList.map(({ name }) => `<b>${name}</b>`).join('\n')}`)
+    ctx.replyWithHTML(`Список названий:\n\n${sprintNamesList.map(({ name }, idx) => `<b>${idx + 1}) ${name}</b>`).join('\n')}\n\nЧтобы предложить своё название введите команду /add`)
   }
 }
 
